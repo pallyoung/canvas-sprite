0a2202b04e15f05a7ca2ac6f2e5dcda5b2186d6f
Spencer <yeshaopeng@lianqin360.com>
2017-09-13 19:06:45 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sh = require('shelljs');
 
 gulp.task('release',function(){
 
 });
 gulp.task('publish',function(){
 
+});
+
+gulp.task('test',function(){
+    sh.rm('-rf','dist');
+    sh.exec('tsc');
+    sh.mv('dist/canvas-sprite.js','demo/canvas-sprite.js')
 });
