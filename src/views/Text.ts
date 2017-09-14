7de247b0fd342e00e69ef7ae586028472fdd3291
Spencer <yeshaopeng@lianqin360.com>
2017-09-14 11:34:02 +0800

    text

--- src/views/Text.ts
+++ src/views/Text.ts
@@ -4,8 +4,19 @@
 namespace cs{
     export namespace view{
         export class Text extends View{
+
+            private $text:string;
+            private $color:string;
+            private $textAlign:string;
+            private $verticalAlign:string;
+            private $font:string;
+            private $size:number;
+
             constructor(){
                 super();
+            }
+            onMeasure(ow:number,oh:number):void{
+                
             }
             onDraw():void{
         
